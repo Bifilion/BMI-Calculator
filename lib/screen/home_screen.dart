321f3ecc298990608edd47f0fbabfc35c9c3ddc0
Jiri Plic <jiriplic@seznam.cz>
2022-09-17 22:24:30 +0200

    Create BMI Calculator main file, create home_screen, age_wight_widget,height_widget,gender_widget

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,72 @@
+import 'package:bmi_calculator/widgets/age_weight_widget.dart';
+import 'package:bmi_calculator/widgets/gender_widget.dart';
+import 'package:bmi_calculator/widgets/height_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _gender = 0;
+  int _height = 150;
+  int _age = 30;
+  int _weight = 50;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("BMI Calculator"),
+      ),
+      body: SingleChildScrollView(
+          child: Container(
+        padding: const EdgeInsets.all(12),
+        child: Card(
+          elevation: 12,
+          shape: const RoundedRectangleBorder(),
+          child: Column(
+            children: [
+              GenderWidget(
+                onChange: (genderVal) {
+                  _gender = genderVal;
+                },
+              ),
+              HeightWidget(
+                onChange: (heightVal) {
+                  _height = heightVal;
+                },
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  AgeWeightWidget(
+                      onChange: (ageVal) {
+                        _age = ageVal;
+                      },
+                      title: "Age",
+                      initValue: 30,
+                      min: 0,
+                      max: 100),
+                  AgeWeightWidget(
+                      onChange: (weightVal) {
+                        _weight = weightVal;
+                      },
+                      title: "Weight(kg)",
+                      initValue: 50,
+                      min: 0,
+                      max: 200)
+                ],
+              )
+            ],
+          ),
+        ),
+      )),
+    );
+  }
+}
